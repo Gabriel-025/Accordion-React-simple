36c2fadc979fd96bcd895f14e307785ab63055b1
Gabriel-025 <78861515+Gabriel-025@users.noreply.github.com>
2022-06-29 10:28:32 -0300

    changed-question

--- src/components/AccorFaq/data.js
+++ src/components/AccorFaq/data.js
@@ -5,17 +5,17 @@ const items = [
       "Clique no ícone do seu perfil e, em seguida, em “MEUS EVENTOS”<br/><br/><p>passo 1: aqui você tem o passo 1</p> <br/><p>passo 2: aqui você tem o passo 1 </p> ",
   },
   {
-    title: "Cowmo fazer o check in do usuário pela plataforma.",
+    title: "Como fazer o check in do usuário pela plataforma...",
     content:
       "Clique no ícone do seu perfil e, em seguida, em “MEUS EVENTOS”<br/><br/><p>passo 1: aqui você tem o passo 1</p> <br/><p>passo 2: aqui você tem o passo 1 </p> ",
   },
   {
-    title: "Coewmo fazer o check in do usuário pela plataforma.",
+    title: "Como fazer o check in do usuário pela plataforma.",
     content:
       "Clique no ícone do seu perfil e, em seguida, em “MEUS EVENTOS”<br/><br/><p>passo 1: aqui você tem o passo 1</p> <br/><p>passo 2: aqui você tem o passo 1 </p> ",
   },
   {
-    title: "Coeemo fazer o check in do usuário pela plataforma.",
+    title: "Como fazer o check in do usuário pela plataforma..",
     content:
       "Clique no ícone do seu perfil e, em seguida, em “MEUS EVENTOS”<br/><br/><p>passo 1: aqui você tem o passo 1</p> <br/><p>passo 2: aqui você tem o passo 1 </p> ",
   },
